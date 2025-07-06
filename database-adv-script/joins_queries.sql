90aa93c7f48f79480ada8a2fbb6d5ee6d4059204
yugorpaul <nhantumboyugor@gmail.com>
2025-07-06 19:09:38 +0200

    Added order by

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -10,15 +10,16 @@ SELECT
 FROM Booking b
 INNER JOIN "User" u ON b.user_id = u.user_id;
 
--- LEFT JOIN: Properties with their reviews
+-- LEFT JOIN: Properties with their reviews (including properties without reviews)
 SELECT 
     p.property_id,
-    p.name,
+    p.name AS property_name,
     r.review_id,
     r.rating,
     r.comment
 FROM Property p
-LEFT JOIN Review r ON p.property_id = r.property_id;
+LEFT JOIN Review r ON p.property_id = r.property_id
+ORDER BY p.property_id;
 
 -- FULL OUTER JOIN: All users and bookings
 SELECT 
