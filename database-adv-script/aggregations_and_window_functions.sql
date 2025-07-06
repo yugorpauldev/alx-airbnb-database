04a8a48af7bbaa116a3b62029906bf0da68f72ab
yugorpaul <nhantumboyugor@gmail.com>
2025-07-06 19:24:09 +0200

    Fix error

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,13 +9,14 @@ LEFT JOIN Booking b ON u.user_id = b.user_id
 GROUP BY u.user_id, u.first_name, u.last_name
 ORDER BY total_bookings DESC;
 
--- Task 2.2: Rank properties based on the total number of bookings
+-- Task 2.2: Rank properties based on the total number of bookings using ROW_NUMBER and RANK
 SELECT 
     p.property_id,
     p.name,
     COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_number_rank,
     RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM Property p
 LEFT JOIN Booking b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name
-ORDER BY booking_rank;
+ORDER BY row_number_rank;
